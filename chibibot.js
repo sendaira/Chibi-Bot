7f586a9024f6e7792b83d74ceb177c0a7d384347
Sendaira Alkazar <sendairaalkazare@gmail.com>
2017-10-22 01:26:45 +0000

    prefixes

--- chibibot.js
+++ chibibot.js
@@ -1,17 +1,16 @@
 const Discord = require("chibibot.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-const prefix = "!";
+// Set the prefix
+let prefix = "!";
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   } else
-
-  if (message.content.startsWith("foo")) {
+  if (message.content.startsWith(prefix + "foo")) {
     message.channel.send("bar!");
   }
 });
