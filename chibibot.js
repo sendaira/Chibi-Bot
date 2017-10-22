0125e3f80e2eb79ccb59220d0dc7bedd3f9d0efb
Sendaira Alkazar <sendairaalkazare@gmail.com>
2017-10-22 01:17:35 +0000

    prefix added

--- chibibot.js
+++ chibibot.js
@@ -1,13 +1,18 @@
-const Discord = require("chibi-bot.js");
+const Discord = require("chibibot.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
+const prefix = "!";
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
+  } else
+
+  if (message.content.startsWith("foo")) {
+    message.channel.send("bar!");
   }
 });
 
