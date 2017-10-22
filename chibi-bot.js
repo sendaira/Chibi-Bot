8b051c06b13459233aaa41f6238b40961e4be9d5
Sendaira Alkazar <sendairaalkazare@gmail.com>
2017-10-22 00:32:16 +0000

    changed  name

--- chibi-bot.js
+++ chibi-bot.js
@@ -14,20 +14,20 @@ client.on("message", (message) => {
 client.login("SuperSecretBotTokenHere");
 
 // This will create the webhook with the name "Example Webhook" and an example avatar.
-message.channel.createWebhook("Example Webhook", "https://i.imgur.com/p2qNFag.png")
+message.channel.createWebhook("Ruling Chibi", "https://i.pinimg.com/originals/e4/46/0d/e4460d4eab8b35ffc1d8e5e336d0f64d.png")
 // This will actually set the webhooks avatar, as mentioned at the start of the guide.
-.then(webhook => webhook.edit("Example Webhook", "https://i.imgur.com/p2qNFag.png")
+.then(webhook => webhook.edit("Ruling Chibi", "https://i.pinimg.com/originals/e4/46/0d/e4460d4eab8b35ffc1d8e5e336d0f64d.png")
 // This will get the bot to DM you the webhook, if you use this in a selfbot,
 // change it to a console.log as you cannot DM yourself
-.then(wb => message.author.send(`Here is your webhook https://canary.discordapp.com/api/webhooks/${wb.id}/${wb.token}`)).catch(console.error))
+.then(wb => message.author.send(`Here is your webhook https://discordapp.com/api/webhooks/371418792745762816/qhJz_z5ek8_LCO5yPGjsOHAcEfNlKwPb_G9yUwWC8o0SS4XXIUVKMrkzqb4Bm6yntaXy/${wb.id}/${wb.token}`)).catch(console.error))
 
 let prefix = "~";
 client.on("message", message => {
   let args = message.content.split(" ").slice(1);
   if (message.content.startsWith(prefix + "createHook")) {
-    message.channel.createWebhook("Example Webhook", "https://i.imgur.com/p2qNFag.png")
-      .then(webhook => webhook.edit("Example Webhook", "https://i.imgur.com/p2qNFag.png")
-        .then(wb => message.author.send(`Here is your webhook https://canary.discordapp.com/api/webhooks/${wb.id}/${wb.token}`))
+    message.channel.createWebhook("Chibi Ruler", "https://i.imgur.com/p2qNFag.png")
+      .then(webhook => webhook.edit("Chibi Ruler", "https://i.imgur.com/p2qNFag.png")
+        .then(wb => message.author.send(`Here is your webhook https://discordapp.com/api/webhooks/371418792745762816/qhJz_z5ek8_LCO5yPGjsOHAcEfNlKwPb_G9yUwWC8o0SS4XXIUVKMrkzqb4Bm6yntaXy/${wb.id}/${wb.token}`))
         .catch(console.error))
       .catch(console.error);
   }
